d91baf8b9881304ad057bc413d0af2885c5b4c24
MassFuer <maxime.ducamp@gmail.com>
2025-12-09 17:12:47 +0100

    implementing components structure

--- kanban-board/src/App.jsx
+++ kanban-board/src/App.jsx
@@ -1,12 +1,18 @@
-import './App.css'
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/KanbanBoard";
+import KanbanBoard from "./components/KanbanBoard";
+import Footer from "./components/Footer";
 
 function App() {
-
   return (
     <>
-
+      <Navbar />
+      <Sidebar />
+      <KanbanBoard />
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
