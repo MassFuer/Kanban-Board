84012438e39fa2dd9fb24d1a57e12c53d7d12272
MassFuer <maxime.ducamp@gmail.com>
2025-12-17 17:15:11 +0100

    styles again

--- kanban-board/src/pages/AboutPage.jsx
+++ kanban-board/src/pages/AboutPage.jsx
@@ -1,21 +1,32 @@
 import { NavLink } from "react-router-dom";
+import { FaGithub } from "react-icons/fa"; // Font Awesome's GitHub icon
+import { FaLinkedin } from "react-icons/fa";
 
 function AboutPage() {
   return (
     <>
       <div className="project">
         <h1>Project Description</h1>
-        <p>Team Member:</p>
+        <p>Super Kanban to manage your tasks</p>
+        <p>Team Member :</p>
         <div className="members">
           <div className="profile-links">
             <span>Amrit</span>
-            <NavLink to="">Link to Github</NavLink>
-            <NavLink to="">Link to LinkedIn</NavLink>
+            <NavLink to="https://github.com/amritbiswal">
+              <FaGithub />
+            </NavLink>
+            <NavLink to="https://www.linkedin.com/in/amritbiswal/">
+              <FaLinkedin />
+            </NavLink>
           </div>
           <div className="profile-links">
             <span>Maxime</span>
-            <NavLink to="">Link to Github</NavLink>
-            <NavLink to="">Link to LinkedIn</NavLink>
+            <NavLink to="https://github.com/MassFuer">
+              <FaGithub />
+            </NavLink>
+            <NavLink to="https://www.linkedin.com/in/maximeducamp/">
+              <FaLinkedin />
+            </NavLink>
           </div>
         </div>
       </div>
